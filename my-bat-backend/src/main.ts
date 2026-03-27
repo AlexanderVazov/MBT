2010fb1459df7352465edd2c315cff8e18643148
viktoriahristova <viktoriya.h.gancheva.2022@elsys-bg.org>
2026-03-28 01:29:27 +0200

    fix of invite

--- my-bat-backend/src/main.ts
+++ my-bat-backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ origin: true, credentials: true });
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', { exclude: ['invite', '.well-known/assetlinks.json'] });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
